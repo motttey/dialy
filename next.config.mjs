f97aa864ab76487e896d53c30ed1b3f3fd119d96
motttey <motitago@gmail.com>
2024-04-30 21:38:56 +0900

    add: next.config.js

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+export const images = {
+    remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'motttey.github.io',
+            port: '',
+            pathname: '**',
+        },
+    ],
+};
