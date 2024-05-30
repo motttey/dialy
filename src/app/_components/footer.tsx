ed59742b0eea3a6a4c66a4c3f1bd3160dcba8516
motttey <motitago@gmail.com>
2024-05-30 22:52:54 +0900

    fix: footer styles

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -2,7 +2,7 @@ import Container from "@/app/_components/container";
 
 export function Footer() {
   return (
-    <footer className="bg-neutral-50 border-t border-[#22b8cf] max-w-4xl mx-auto">
+    <footer className="bg-neutral-50 border-t-4 border-[#22b8cf] max-w-4xl mx-auto">
       <Container>
         <div className="py-4 flex flex-col lg:flex-row items-center">
           <h3 className="text-2xl lg:text-[1.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
