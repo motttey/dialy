4f201fc6dcb00f9c5f6cba508ec4bcb726167297
motttey <motitago@gmail.com>
2024-06-15 10:25:28 +0900

    fix: hide title, alert

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -1,17 +1,17 @@
 import Avatar from "./avatar";
 import CoverImage from "./cover-image";
-import DateFormatter from "./date-formatter";
+// import DateFormatter from "./date-formatter";
 import { PostTitle } from "@/app/_components/post-title";
 import { type Author } from "@/interfaces/author";
 
 type Props = {
   title: string;
   coverImage: string;
-  date: string;
+  // date: string;
   author: Author;
 };
 
-export function PostHeader({ title, coverImage, date, author }: Props) {
+export function PostHeader({ title, coverImage, author }: Props) {
   return (
     <div className="max-w-3xl mx-auto">
       <PostTitle>{title}</PostTitle>
@@ -25,9 +25,11 @@ export function PostHeader({ title, coverImage, date, author }: Props) {
         <div className="block md:hidden mb-6">
           <Avatar name={author.name} picture={author.picture} />
         </div>
+        {/*
         <div className="mb-6 text-lg">
           <DateFormatter dateString={date} />
         </div>
+        */}
       </div>
     </div>
   );
