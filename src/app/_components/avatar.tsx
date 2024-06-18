277f7382dc5fc0f4cd67b6a9e99f0ab6baf38f64
motttey <motitago@gmail.com>
2024-06-19 07:23:48 +0900

    fix: use next/image for avaters

--- src/app/_components/avatar.tsx
+++ src/app/_components/avatar.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type Props = {
   name: string;
   picture: string;
@@ -6,7 +8,7 @@ type Props = {
 const Avatar = ({ name, picture }: Props) => {
   return (
     <div className="flex items-center">
-      <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      <Image src={picture} width={50} height={50} className="w-12 h-12 rounded-full mr-4" alt={name} unoptimized={true}/>
       <div className="text-lg font-bold">{name}</div>
     </div>
   );
