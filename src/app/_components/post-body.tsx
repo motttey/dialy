86fa7a75a7aec4d07119ed2bb8e37a3bf601dd61
motttey <motitago@gmail.com>
2024-06-27 19:56:30 +0900

    modify: post format

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -1,12 +1,22 @@
+import { Noto_Serif_JP } from "next/font/google";
 import markdownStyles from "./markdown-styles.module.css";
 
 type Props = {
   content: string;
 };
 
+const NotoSerifJP = Noto_Serif_JP({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  // https://github.com/vercel/next.js/pull/44594
+  variable: "--font-noto-sans-jp",
+  preload: true,
+  display: "swap",
+});
+
 export function PostBody({ content }: Props) {
   return (
-    <div className="max-w-4xl mx-auto">
+    <div className={`max-w-4xl mx-auto ${NotoSerifJP.className}`}>
       <div
         className={markdownStyles["markdown"]}
         dangerouslySetInnerHTML={{ __html: content }}
