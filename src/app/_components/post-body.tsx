53733a6329c7f2a371c424a5d97a6ba040817177
motttey <motitago@gmail.com>
2024-07-28 20:44:12 +0900

    fix: modify styles max-width

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -16,7 +16,7 @@ const NotoSerifJP = Noto_Serif_JP({
 
 export function PostBody({ content }: Props) {
   return (
-    <div className={`max-w-4xl mx-auto ${NotoSerifJP.className}`}>
+    <div className={`mx-auto max-w-full ${NotoSerifJP.className}`}>
       <div
         className={markdownStyles["markdown"]}
         dangerouslySetInnerHTML={{ __html: content }}
