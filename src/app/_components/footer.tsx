d0748c934dc81faa71af0d37a6285293a2e7e940
motttey <motitago@gmail.com>
2024-09-15 22:08:11 +0900

    fix: footer margin

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer>
       <Container>
-        <div className="py-4 border-t-4 border-[#22b8cf] text-center mx-auto px-8 lg:px-16">
+        <div className="py-4 border-t-4 border-[#22b8cf] text-center max-w-4xl mx-auto px-8 lg:px-16">
           <h3 className="text-2xl lg:text-[1.5rem] font-bold tracking-tighter leading-tight text-center mb-4 lg:mb-8">
             Mochidu-ki
           </h3>
