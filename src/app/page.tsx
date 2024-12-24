3629add158cd137f39eda9bc1380305bac52d860
motttey <motitago@gmail.com>
2024-12-24 20:34:17 +0900

    fix: toppage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import { Archives } from "@/app/_components/archives";
 import Container from "@/app/_components/container";
 import CoverImage from "@/app/_components/cover-image";
 import { Header } from "@/app/_components/header";
-import { PostBody } from "@/app/_components/post-body";
+// import { PostBody } from "@/app/_components/post-body";
 import { getAllPosts } from "@/lib/api";
-import markdownToHtml from "@/lib/markdownToHtml";
+// import markdownToHtml from "@/lib/markdownToHtml";
 
 export default async function Index() {
   const allPosts = getAllPosts();
@@ -14,7 +14,7 @@ export default async function Index() {
   const recentPost = sortedList[0];
 
   const pastPosts = allPosts.slice(0);
-  const firstContent = await markdownToHtml(recentPost.content || "");
+  // const firstContent = await markdownToHtml(recentPost.content || "");
 
   return (
     <main>
@@ -26,7 +26,10 @@ export default async function Index() {
             title={recentPost.title}
             src={recentPost.coverImage}
           />
-          <PostBody content={firstContent} />
+          {/* <PostBody content={firstContent} /> */}
+          <div className={"mx-auto max-w-full lg:max-w-4xl"}>
+            終了です. さようなら
+          </div>
         </div>
         {pastPosts.length > 0 && <Archives posts={pastPosts} />}
       </Container>
