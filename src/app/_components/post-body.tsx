80e4ac26d42ca65c4ec9f1080c7d9c37c27685fb
motttey <motitago@gmail.com>
2025-02-11 08:40:17 +0900

    modify: parse markdown using dompurify

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -1,4 +1,6 @@
 import markdownStyles from "./markdown-styles.module.css";
+import DOMPurify from "dompurify";
+import { JSDOM } from "jsdom";
 import { Noto_Serif_JP } from "next/font/google";
 
 type Props = {
@@ -14,12 +16,18 @@ const NotoSerifJP = Noto_Serif_JP({
   display: "swap",
 });
 
+// https://github.com/cure53/DOMPurify?tab=readme-ov-file#running-dompurify-on-the-server
 export function PostBody({ content }: Props) {
+  const window = new JSDOM("").window;
+  const purify = DOMPurify(window);
+
   return (
     <div className={`mx-auto max-w-full lg:max-w-4xl ${NotoSerifJP.className}`}>
       <div
         className={markdownStyles["markdown"]}
-        dangerouslySetInnerHTML={{ __html: content }}
+        dangerouslySetInnerHTML={{
+          __html: purify.sanitize(content),
+        }}
       />
     </div>
   );
