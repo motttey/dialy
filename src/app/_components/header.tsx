18fd57763e401768c71591e1b89124a6a700f55f
motttey <motitago@gmail.com>
2025-07-24 20:11:00 +0900

    fix: link

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export function Header() {
   return (
     <section className="mx-auto mb-8 mt-16 flex max-w-4xl flex-col items-center border-b-4 border-[#22b8cf] pb-4 md:mb-12 md:flex-row md:justify-between">
-      <h1 className="text-4xl font-bold leading-tight tracking-tighter md:pr-8 md:text-6xl">
-        モチヅ記
-      </h1>
+      <Link href={`/`}>
+        <h1 className="text-4xl font-bold leading-tight tracking-tighter md:pr-8 md:text-6xl">
+          モチヅ記
+        </h1>
+      </Link>
       <h4 className="mt-5 text-center text-lg md:pl-8 md:text-left">
         A dialy written by{" "}
         <a
