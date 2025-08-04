b158d9f41c60671fb1e042309df9bf30e097bee3
motttey <motitago@gmail.com>
2025-08-04 21:02:20 +0900

    update: add thumb images for memo

--- src/app/memo/page.tsx
+++ src/app/memo/page.tsx
@@ -1,8 +1,10 @@
 import Container from "@/app/_components/container";
+import DateFormatter from "@/app/_components/date-formatter";
 import { Header } from "@/app/_components/header";
 import PostService from "@/lib/api";
+
+import Image from "next/image";
 import Link from "next/link";
-import DateFormatter from "../_components/date-formatter";
 
 const MEMO_DIR = "_memo";
 const postService = new PostService(MEMO_DIR);
@@ -17,12 +19,28 @@ export default async function Index() {
         <h1 className="my-4 text-3xl font-bold">雑記</h1>
         <ul>
           {memos.map((memo) => (
-            <li key={memo.slug} className="mb-2">
+            <li key={memo.slug} className="mb-4">
               <Link
+                className="flex items-center gap-4 text-lg hover:no-underline"
                 href={`/memo/${memo.slug}`}
-                className="text-lg hover:underline"
               >
-                {memo.title} [{<DateFormatter dateString={memo.date} />}]
+                <Image
+                  className="rounded-lg object-cover"
+                  src={
+                    memo.coverImage ||
+                    "https://motttey.github.io/404notfound.webp"
+                  }
+                  alt={`Cover Image for ${memo.title}`}
+                  width={150}
+                  height={80}
+                  unoptimized={true}
+                />
+                <div className="flex flex-col">
+                  <span className="font-bold hover:underline">
+                    {memo.title}
+                  </span>
+                  <DateFormatter dateString={memo.date} />
+                </div>
               </Link>
             </li>
           ))}
