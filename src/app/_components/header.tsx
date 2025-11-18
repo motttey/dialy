ac6dff3d6062cadf186b88aefc691910e54b2008
motttey <motitago@gmail.com>
2025-11-18 21:36:51 +0900

    migrate: tailwind to v4

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <section className="mx-auto mb-8 mt-16 flex max-w-4xl flex-col items-center border-b-4 border-[#22b8cf] pb-4 md:mb-12 md:flex-row md:justify-between">
       <Link href={`/`}>
-        <h1 className="text-4xl font-bold leading-tight tracking-tighter md:pr-8 md:text-6xl">
+        <h1 className="text-4xl font-bold leading-tight tracking-tighter md:pr-8 md:text-5xl">
           モチヅ記
         </h1>
       </Link>
